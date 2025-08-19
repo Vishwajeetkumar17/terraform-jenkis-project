0c26739a4f67e3666c3a32d15855e2459dab9d69
Vishwajeet kumar <117035670+Vishwajeetkumar17@users.noreply.github.com>
2025-08-19 21:01:54 +0530

    Add files via upload

---
+++ elb.tf
@@ -0,0 +1,26 @@
+resource "aws_elb" "bar" {
+  name               = "vishwajeet-terraform-elb"
+  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 5
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout              = 400
+  tags = {
+    Name = "vishwajeet-tf-elb"
+  }
+}
